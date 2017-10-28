abfeb2b4c10c96b589b7a2eea6f193259faaf40f
Kenia Isabel Delgado Flores <a291450@uach.mx>
2017-10-28 08:21:12 -0600

    Se agregan las funciones para las rutas de fecha,mapa y chihuahua

--- controlers/publicaciones/publicaciones.js
+++ controlers/publicaciones/publicaciones.js
@@ -5,6 +5,24 @@ function index(req, res, next){
   res.render('publicaciones/publicaciones',{});
 }
 
+function fecha(req, res, next){
+  //res.send("Hola soy la ruta publicacion");
+  res.render('publicaciones/fecha',{});
+}
+
+function mapa(req, res, next){
+  //res.send("Hola soy la ruta publicacion");
+  res.render('publicaciones/mapa',{});
+}
+
+function chihuahua(req, res, next){
+  //res.send("Hola soy la ruta publicacion");
+  res.render('publicaciones/chihuahua',{});
+}
+
 module.exports = {
-  index
+  index,
+  fecha,
+  mapa,
+  chihuahua
 }
