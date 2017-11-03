11dda5d5abb5c44617d53abb9d457051d07670ea
Ararat Mendez <ararat.mendez@gmail.com>
2017-11-03 00:53:29 -0700

    libreria materialize

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -2,6 +2,24 @@ $(document).ready(function() {
 
     $('#hamburguer').on('click', function() {
         this.classList.toggle('active');
-    })
+    });
+
+    $(".drag-target").click(function(){
+        $("#hamburguer").toggleClass("active");
+    });
+
+    $(".drag-target").click(function(){
+        $(".drag-target").removeClass("d-initial");
+    });
+
+    $("#hamburguer").click(function(){
+        $(".drag-target").addClass("d-initial");
+    });
+
+    $("#hamburguer").click(function(){
+        $(".side-nav").toggleClass("d-initial");
+    });
 
 });
+
+$(".button-collapse").sideNav();
