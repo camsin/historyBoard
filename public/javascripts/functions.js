34d7a8507ace7b216cfa5d7e21bccec742d56e22
Ararat Mendez <ararat.mendez@gmail.com>
2017-11-04 00:43:02 -0700

    imagenes random

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -36,6 +36,6 @@ tippy('.tippy', {
 })
 
 $(function() {
-    var images = ['login.jpg', 'plaza.jpg'];
+    var images = ['login.jpg', 'plaza.jpg', '1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg',];
     $('.img-header').css({'background-image': 'url(../../images/' + images[Math.floor(Math.random() * images.length)] + ')'});
 });
