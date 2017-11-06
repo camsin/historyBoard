aad646fdd67a82dfa9d7c6661eb76441248ddfe5
Ararat Mendez <ararat.mendez@gmail.com>
2017-11-05 22:42:31 -0700

    Se corrige controlador de publicaciones

--- controllers/publicaciones/publicaciones.js
+++ controllers/publicaciones/publicaciones.js
@@ -1,19 +1,44 @@
 const express = require('express')
 
-function index(req, res, next){
-  res.render('publicaciones/publicaciones',{});
+function mapa(req, res, next){
+    res.render('publicaciones/mapa', {showSideNav: true});
 };
 
-
 function byEstado(req, res, next){
-  router.get('/byEstado', (req, res, next) => {
     res.render('publicaciones/byEstado', {
-      title: 'publicaciones',
-      state: 'Chihuahuita'});
-  });
+        estado:req.params.estado, showSideNav: true});
+};
+
+function byFecha(req, res, next){
+    res.render('publicaciones/byFecha', { showSideNav: true});
+
+};
+
+function ultimasPublicaciones(req, res, next){
+    res.render('publicaciones/ultimasPublicaciones',{showSideNav: true});
+
+};
+
+function byId(req, res, next){
+    res.render('publicacion/byId', {
+        id: req.params.id, showSideNav: true});
+};
+
+function nueva(req, res, next){
+    res.render('publicacion/nueva', {showSideNav: true,
+        contenido: req.params.contenido});
+};
+
+function misPublicaciones(req, res, next){
+    res.render('publicaciones/misPublicaciones', { showSideNav: true, title: 'misPublicaciones' });
 };
 
 module.exports = {
-  index,
-  byEstado
+  mapa,
+  byEstado,
+  byFecha,
+  ultimasPublicaciones,
+  byId,
+  nueva,
+  misPublicaciones
 };
