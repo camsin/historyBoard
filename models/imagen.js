8b7d4dcc55ffbb35f8cf5a0551ef54df40767634
Kenia Delgado <a291450@uach.mx>
2017-11-16 14:47:10 -0700

    Agregando campos necesarios para almacenar imagenes

--- models/imagen.js
+++ models/imagen.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
 const schema = mongoose.Schema;
-
+/**
+ * Modelo de las imagenes manejadas en nuestra aplicación
+ */
  const imagenSchema = schema({
-   file_id: String
+   file_id: String,
+   img: {
+     data: Buffer,
+     contentType: String
+   }
  });
 
  module.exports = mongoose.model('Imagen',imagenSchema);
