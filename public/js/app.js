c914d3dde364820ac43066293e76797fca4ef110
Ararat Mendez <ararat.mendez@gmail.com>
2017-11-19 17:31:16 -0700

    Se agrega angular

---
+++ public/js/app.js
@@ -0,0 +1,9 @@
+
+var app = angular.module('historyBoardApp', []);
+
+app.controller('usersController', ['$scope', '$http', function ($scope, $http) {
+
+    $scope.getUser = function (idUser) {
+        console.log("user", idUser);
+    };
+}]);
