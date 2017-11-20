2f7a6acb0bf4b91a59787446db7b1139d206d01d
Kenia delgado <a291450@uach.mx>
2017-11-19 20:26:14 -0700

    Comentarios breves de cada funcion

--- controllers/publicaciones/publicaciones.js
+++ controllers/publicaciones/publicaciones.js
@@ -3,31 +3,44 @@ const Imagen = require('../../models/imagen');
 const Publicacion = require('../../models/publicacion');
 var fs = require('fs');
 
-
+/**
+ * Funcion para mostrar/interactuar con el mapa
+ */
 function mapa(req, res, next){
     res.render('publicaciones/mapa', {showSideNav: true});
 };
 
+/**
+ * Función para hacer nuestro filtrado de publicaciones por estado
+ */
 function byEstado(req, res, next){
     res.render('publicaciones/byEstado', {
         estado:req.params.estado, showSideNav: true});
 };
-
+/**
+ * Función para hacer nuestro filtrado de publicaciones por fecha/año
+ */
 function byFecha(req, res, next){
     res.render('publicaciones/byFecha', { showSideNav: true});
 
 };
-
+/**
+ * Función que nos nuestra la página principal de ultimasPublicaciones
+ */
 function ultimasPublicaciones(req, res, next){
     res.render('publicaciones/ultimasPublicaciones',{showSideNav: true});
 
 };
-
+/**
+ * Función que nos ayuda a mostrar el contenido de una publicación específica
+ */
 function byId(req, res, next){
     res.render('publicacion/byId', {
         id: req.params.id, showSideNav: true});
 };
-
+/**
+ * Función que nos permite la creación de nuevas publicaciones
+ */
 function nueva(req, res, next){
 
   if(req.params.contenido == 2){
@@ -58,8 +71,9 @@ function nueva(req, res, next){
   }
 };
 
-
-
+/**
+ * Función que pŕactica
+ */
 function test(req, res, next){
 
  if(req.params.contenido == 2){
@@ -97,7 +111,9 @@ function test(req, res, next){
 }
 
 };
-
+/**
+ * Función que nos ayuda a mostrar las publicaciones especificas del usuario conectado
+ */
 function misPublicaciones(req, res, next){
     res.render('publicaciones/misPublicaciones', { showSideNav: true, title: 'misPublicaciones' });
 };
