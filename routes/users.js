bba5525e076d726beedaef604ce551ee15a9c1c3
Kenia delgado <a291450@uach.mx>
2017-11-25 19:35:14 -0700

    Se implementa multer para poder enviar imagen de perfil

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 const usersController = require('../controllers/users/users');
 const isLoggedIn = require('./../auth/passport.js').isLoggedIn;
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+let type = upload.any();
 
 
 /* GET home page. */
@@ -9,6 +12,6 @@ router.get('/myProfile', isLoggedIn, usersController.myProfile);
 
 router.get('/getMyProfile', isLoggedIn, usersController.getMyProfile);
 
-router.post('/updateMyProfile', isLoggedIn, usersController.updateMyProfile);
+router.post('/updateMyProfile', isLoggedIn, type, usersController.updateMyProfile);
 
 module.exports = router;
