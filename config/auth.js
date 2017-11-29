21d73a1d4ef630a5c345a49e700f19e8626823f1
Ararat Mendez <ararat.mendez@gmail.com>
2017-11-29 02:56:21 -0700

    Se cambia la ruta de auth

--- config/auth.js
+++ config/auth.js
@@ -9,13 +9,13 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '356249854732540', // your App ID
         'clientSecret'  : '98193a8c62c6ab1241d71f0e50b72c4e', // your App Secret
-        'callbackURL'   : 'http://localhost:3000/auth/facebook/callback'
+        'callbackURL'   : 'https://history-board.herokuapp.com/auth/facebook/callback'
     },
 
     'googleAuth' : {
         'clientID'      : '102853990433-ptbpublucjfifnt9s5nt3i28hvsgrvmd.apps.googleusercontent.com',
         'clientSecret'  : 'sfHNkgr0gbDm8E0-Xr11-G9E',
-        'callbackURL'   : 'http://localhost:3000/auth/google/callback'
+        'callbackURL'   : 'https://history-board.herokuapp.com/auth/google/callback'
     }
 
 };
