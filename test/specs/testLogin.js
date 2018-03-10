2c79e48e213184222165cd7d078fe09e8e556ab1
Ararat Mendez <ararat.mendez@gmail.com>
2018-03-10 15:20:27 -0700

    Se agregan pruebas de editar perfil

--- test/specs/testLogin.js
+++ test/specs/testLogin.js
@@ -1,51 +1,51 @@
 
-
 const expect = require('chai').expect;
+const path = require('path');
 
 
 
-describe('history board pruebas de Registrarse y Entrar',function(){
+describe('Pruebas de Registrarse y Entrar',function(){
     browser.url('http://localhost:3000');
 
     //Prueba de registrar email que ya esta en uso
-    it('Debe mostrar toast con: email already in use. Si el email esta en uso',function(){
+    it('Prueba de mail en uso. Debe mostrar toast con: email already in use.',function(){
         if(browser.isExisting('.login-view')){
 
             browser.click('.btn-blue');
-            browser.waitForVisible('.register-modal', 999999);
+            browser.waitForVisible('.register-modal', 30000);
             browser.setValue(' .name-register-test ','Carolina');
             browser.setValue('.email-register-test ','connie@gmail.com');
             browser.setValue('.password-register-test ','123');
             browser.setValue('.confirm-password-register-test ','123');
             browser.click('.waves-light');
-            browser.waitForExist('.alert',999999);
+            browser.waitForExist('.alert',30000);
             expect(browser.isExisting('.alert-danger')).to.be.true;
         }
     });
 
     //Prueba de registrar contrasenas que no son iguales
-    it('Debe mostrar toast con: Passwords are not equals. Si las contrasenas no son iguales',function(){
+    it('Prueba de contraseñas diferentes. Debe mostrar toast con: Passwords are not equals.',function(){
         if(browser.isExisting('.login-view')){
 
             browser.click('.btn-blue');
-            browser.waitForVisible('.register-modal', 999999);
+            browser.waitForVisible('.register-modal', 30000);
             browser.setValue(' .name-register-test ','Carolina');
             browser.setValue('.email-register-test ','soyunemail@gmail.com');
             browser.setValue('.password-register-test ','123');
             browser.setValue('.confirm-password-register-test ','123qwqert');
             browser.click('.waves-light');
-            browser.waitForExist('.alert',999999);
+            browser.waitForExist('.alert',30000);
             expect(browser.isExisting('.alert-danger')).to.be.true;
         }
     });
 
     //CAMBIAR O ELIMINAR LOS DATOS DE LA BD/////////////////////
     //Prueba de crear usuario
-    it('Crear usuario',function(){
+    it('Prueba de crear usuario',function(){
         if(browser.isExisting('.login-view')){
 
             browser.click('.btn-blue');
-            browser.waitForVisible('.register-modal', 999999);
+            browser.waitForVisible('.register-modal', 30000);
             browser.setValue(' .name-register-test ','Willow');
             browser.setValue('.email-register-test ','willow@gmail.com');
             browser.setValue('.password-register-test ','123');
@@ -56,43 +56,43 @@ describe('history board pruebas de Registrarse y Entrar',function(){
     });
 
     //Prueba de intentar ingresar con nombre incorrecto
-    it('Debe mostrar toast con: Incorrect username. Si el usuario es incorrecto',function(){
+    it('Prueba de usuario incorrecto. Debe mostrar toast con: Incorrect username.',function(){
         if(browser.isExisting('.login-view')){
 
             browser.click('.btn-white');
-            browser.waitForVisible('.login-modal', 999999);
+            browser.waitForVisible('.login-modal', 30000);
             browser.setValue('.email-login-test ','soyunemail@gmail.com');
             browser.setValue('.password-login-test ','123');
             browser.click('.btn-primary');
-            browser.waitForExist('.alert',999999);
+            browser.waitForExist('.alert',30000);
             expect(browser.isExisting('.alert-danger')).to.be.true;
         }
     });
 
     //Prueba de intentar ingresar con contra incorrecta
-    it('Debe mostrar toast con: Incorrect password. si la contra es incorrecta',function(){
+    it('Prueba de contraseña incorrecta. Debe mostrar toast con: Incorrect password.',function(){
         if(browser.isExisting('.login-view')){
 
             browser.click('.btn-white');
-            browser.waitForVisible('.login-modal', 999999);
+            browser.waitForVisible('.login-modal', 30000);
             browser.setValue('.email-login-test ','connie@gmail.com');
             browser.setValue('.password-login-test ','1231092830129ek0w');
             browser.click('.btn-primary');
-            browser.waitForExist('.alert',999999);
+            browser.waitForExist('.alert',30000);
             expect(browser.isExisting('.alert-danger')).to.be.true;
         }
     });
 
     //Prueba de intentar ingresar con campos vacios
-    it('Debe mostrar toast con: Missing credentials. Si no se llenan los campos',function(){
+    it('Prueba de no llenar los campos. Debe mostrar toast con: Missing credentials.',function(){
         if(browser.isExisting('.login-view')){
 
             browser.click('.btn-white');
-            browser.waitForVisible('.login-modal', 999999);
+            browser.waitForVisible('.login-modal', 30000);
             browser.setValue('.email-login-test ','');
             browser.setValue('.password-login-test ','');
             browser.click('.btn-primary');
-            browser.waitForExist('.alert',999999);
+            browser.waitForExist('.alert',30000);
             expect(browser.isExisting('.alert-danger')).to.be.true;
         }
     });
@@ -102,15 +102,102 @@ describe('history board pruebas de Registrarse y Entrar',function(){
         if(browser.isExisting('.login-view')){
 
             browser.click('.btn-white');
-            browser.waitForVisible('.login-modal', 999999);
-            browser.setValue('.email-login-test ','perla@gmail.com');
+            browser.waitForVisible('.login-modal', 30000);
+            browser.setValue('.email-login-test ','willow@gmail.com');
             browser.setValue('.password-login-test ','123');
             browser.click('.btn-primary');
             expect(browser.isExisting('.home-page')).to.be.true;
         }
     });
-})
+});
+
+describe('Pruebas de actualizar perfil',function(){
+    browser.url('http://localhost:3000/publications/lastPublications');
+
+    //Prueba de intentar ingresar con datos correctos
+    it('Prueba de no llenar datos. Debe salir toast de error',function(){
+        if(browser.isExisting('.home-page')){
+
+            browser.click('#hamburguer');
+            browser.click('.perfil-boton');
+            browser.waitForVisible('.name-change-test', 30000);
+            browser.setValue('.name-change-test ','');
+            browser.setValue('.new-psw-change-test ','');
+            browser.setValue('.confirm-new-psw-change-test ','');
+            browser.setValue('.img-change-test','');
+            browser.click('.btn-primary');
+            browser.waitForExist('#toast-container',30000);
+            expect(browser.getText('.toast-error')).to.be.equal('Error\nPara guardar tus cambios debes de ingresar tu contraseña y confirmarla');
+            browser.click('#toast-container');
 
 
+            // expect(browser.isExisting('Para guardar tus cambios debes de ingresar tu contraseña y confirmarla')).to.be.true;
+        }
+    });
 
+    //Prueba de intentar ingresar nombre pero no contraseña
+    it('Prueba de cambiar nombre sin ingresar contraseña. Debe salir toast de error',function(){
+        if(browser.isExisting('.home-page')){
+
+            browser.waitForVisible('.name-change-test', 30000);
+            browser.setValue('.name-change-test ','Karen');
+            browser.setValue('.new-psw-change-test ','');
+            browser.setValue('.confirm-new-psw-change-test ','');
+            browser.setValue('.img-change-test','');
+            browser.click('.btn-primary');
+            browser.waitForExist('#toast-container',30000);
+            expect(browser.getText('.toast-error')).to.be.equal('Error\nPara guardar tus cambios debes de ingresar tu contraseña y confirmarla');
+            browser.click('#toast-container');
+        }
+    });
+
+    //Prueba de intentar ingresar nombre, contraseña y no confirmarla
+    it('Prueba de cambiar nombre sin ingresar  la confirmacion de la contraseña. Debe salir toast de error',function(){
+        if(browser.isExisting('.home-page')){
+
+            browser.waitForVisible('.name-change-test', 30000);
+            browser.setValue('.name-change-test ','Karen');
+            browser.setValue('.new-psw-change-test ','321');
+            browser.setValue('.confirm-new-psw-change-test ','');
+            browser.setValue('.img-change-test','');
+            browser.click('.btn-primary');
+            browser.waitForExist('#toast-container',30000);
+            expect(browser.getText('.toast-error')).to.be.equal('Error\nPara guardar tus cambios debes de ingresar tu contraseña y confirmarla');
+            browser.click('#toast-container');
+        }
+    });
+
+    //Prueba de intentar ingresar nombre, contraseña pero las contraseñas  no son iguales
+    it('Prueba de cambiar nombre y que las contraseñas no sean iguales. Debe salir toast de error',function(){
+        if(browser.isExisting('.home-page')){
+
+            browser.waitForVisible('.name-change-test', 30000);
+            browser.setValue('.name-change-test ','Karen');
+            browser.setValue('.new-psw-change-test ','321');
+            browser.setValue('.confirm-new-psw-change-test ','321321');
+            browser.setValue('.img-change-test','');
+            browser.click('.btn-primary');
+            browser.waitForExist('#toast-container',30000);
+            expect(browser.getText('.toast-error')).to.be.equal('Error\nLas contraseñas no coinciden');
+            browser.click('#toast-container');
+        }
+    });
+
+    //Prueba de cambiar nombre
+    it('Prueba de cambiar nombre',function(){
+        if(browser.isExisting('.home-page')){
+
+            browser.waitForVisible('.name-change-test', 30000);
+            browser.setValue('.name-change-test ','Buffy');
+            browser.setValue('.new-psw-change-test ','321');
+            browser.setValue('.confirm-new-psw-change-test ','321');
+            browser.chooseFile('.img-change-test', path.resolve('./test/specs/', './img/doggo.jpg'));
+            browser.click('.btn-primary');
+            browser.waitForExist('#toast-container',30000);
+            expect(browser.getText('.toast-success')).to.be.equal('Tu perfil se ha actualizado\nCORRECTAMENTE');
+            browser.click('#toast-container');
+        }
+    });
+
+});
 
